2eca31772e18b9b5de2f80c6e01ab37d0b0204c6
Keerthy E B <44020061+keerthyeb@users.noreply.github.com>
2018-11-21 17:09:33 +0530

    Modify mapTest.js

--- test/mapTest.js
+++ test/mapTest.js
@@ -15,22 +15,21 @@ const findLength = function(element){
 
 describe("map", function(){
 
-  it("Testing identity function",function(){
+  it("Should return an empty array if we are giving an empty array",function(){
     assert.deepEqual(map(identity,[]),[]);
-    assert.deepEqual(map(identity,[3]),[3]);
-    assert.deepEqual(map(identity,[1,2,3,4]),[1,2,3,4]);
-    assert.deepEqual(map(identity,["constant"]),["constant"]);
-  });
-
-  it("Testing function that return same dataType", function(){
     assert.deepEqual(map(square,[]),[]);
-    assert.deepEqual(map(square,[3]),[9]);
-    assert.deepEqual(map(square,[1,2,3,4]),[1,4,9,16]);
+    assert.deepEqual(map(findLength,[]),[]);
   });
 
-  it("Testing function that return different dataType",function(){ 
-    assert.deepEqual(map(findLength,[]),[]);
+  it("Should work only if array of one element is given", function(){
+    assert.deepEqual(map(identity,[1]),[1]);
+    assert.deepEqual(map(square,[3]),[9]);
     assert.deepEqual(map(findLength,["keerthy"]),[7]);
+  });
+
+  it("Should work with array of more than one element",function(){ 
+    assert.deepEqual(map(identity,[1,2,3,4]),[1,2,3,4]);
+    assert.deepEqual(map(square,[1,2,3,4]),[1,4,9,16]);
     assert.deepEqual(map(findLength,["puli","keerthy","athul"]),[ 4 , 7 , 5 ]);
   });
 
